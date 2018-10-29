8161249210d41a754425b7ecea3f7d741e26eb09
Rizwan Khan <rizwan.n.khan24@gmail.com>
2018-10-29 10:12:31 -0500

    Initial Commit

---
+++ hustle-mode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hustle-mode
+//
+//  Created by Rizwan Khan on 10/29/18.
+//  Copyright © 2018 Rizwan Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
